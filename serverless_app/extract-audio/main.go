df4a43063f3f02157dc7bc079f4dea24b633705e
ken3pei <kensuke1202left@gmail.com>
2025-02-03 19:16:34 +0900

    最低限の構成にアプリケーションを修正

--- serverless_app/extract-audio/main.go
+++ serverless_app/extract-audio/main.go
@@ -1,53 +1,48 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
-
-	"github.com/aws/aws-sdk-go-v2/config"
-
-	"extract-audio/infra"
 )
 
 func main() {
 	setEnvs()
-	ctx := context.Background()
+	// ctx := context.Background()
 
-	// setup context(1.IAMロールの認証情報取得)
-	cfg, err := config.LoadDefaultConfig(ctx)
-	if err != nil {
-		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
-		fmt.Println(err)
-		return
-	}
-	// download mov file
-	s3Client := infra.NewBucketBasics(cfg)
-	fmt.Println("download mov file")
-	err = s3Client.DownloadFile(ctx, os.Getenv("BUCKET_NAME"), os.Getenv("OBJECT_KEY"), os.Getenv("LOCAL_INPUT"))
-	if err != nil {
-		fmt.Println("Couldn't download movie file from s3client.")
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("complete")
+	// // setup context(1.IAMロールの認証情報取得)
+	// cfg, err := config.LoadDefaultConfig(ctx)
+	// if err != nil {
+	// 	fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// // download mov file
+	// s3Client := infra.NewBucketBasics(cfg)
+	// fmt.Println("download mov file")
+	// err = s3Client.DownloadFile(ctx, os.Getenv("BUCKET_NAME"), os.Getenv("OBJECT_KEY"), os.Getenv("LOCAL_INPUT"))
+	// if err != nil {
+	// 	fmt.Println("Couldn't download movie file from s3client.")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println("complete")
 
-	// ffmpeg command
-	fmt.Println("exec ffmpeg command")
-	execFFmpegCommand(os.Getenv("LOCAL_INPUT"))
-	fmt.Println("complete")
+	// // ffmpeg command
+	// fmt.Println("exec ffmpeg command")
+	// execFFmpegCommand(os.Getenv("LOCAL_INPUT"))
+	// fmt.Println("complete")
 
-	// upload mp3 file
-	fmt.Println("upload mp3 file")
-	err = s3Client.UploadFile(ctx, os.Getenv("BUCKET_NAME"), os.Getenv("S3_UPLOAD"), os.Getenv("LOCAL_OUTPUT"))
-	if err != nil {
-		fmt.Println("Couldn't upload movie file from s3client.")
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("complete")
+	// // upload mp3 file
+	// fmt.Println("upload mp3 file")
+	// err = s3Client.UploadFile(ctx, os.Getenv("BUCKET_NAME"), os.Getenv("S3_UPLOAD"), os.Getenv("LOCAL_OUTPUT"))
+	// if err != nil {
+	// 	fmt.Println("Couldn't upload movie file from s3client.")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println("complete")
 }
 
 func execFFmpegCommand(filename string) {
@@ -75,4 +70,8 @@ func setEnvs() {
 	os.Setenv("LOCAL_INPUT", fmt.Sprintf("/tmp/%s.mp4", filepath))
 	os.Setenv("LOCAL_OUTPUT", fmt.Sprintf("/tmp/%s.mp3", filepath))
 	os.Setenv("S3_UPLOAD", fmt.Sprintf("mp3/%s.mp3", filepath))
+
+	println("LOCAL_INPUT:", os.Getenv("LOCAL_INPUT"))
+	println("LOCAL_OUTPUT:", os.Getenv("LOCAL_OUTPUT"))
+	println("S3_UPLOAD:", os.Getenv("S3_UPLOAD"))
 }
